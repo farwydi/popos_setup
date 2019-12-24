1b384355c583f744168ff16531a8a24c7c30ff58
Leonid Zharikov <leon.zharikov@gmail.com>
2019-12-24 22:29:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Setup git..."
+git config --global user.name "Leonid Zharikov"
+git config --global user.email leon.zharikov@gimail.com
+
 echo "Setup audio..."
 amixer -c 1 set 'Master' 57
 amixer -c 1 set 'HP/Speaker Auto Detect' on
